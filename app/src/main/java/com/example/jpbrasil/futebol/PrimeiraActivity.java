66125886a007f4289a89882ae4d9ad5269e38873
jpcm1978 <jpcm1978@hotmail.com>
2017-05-08 20:22:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jpbrasil/futebol/PrimeiraActivity.java
@@ -0,0 +1,13 @@
+package com.example.jpbrasil.futebol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira);
+    }
+}
