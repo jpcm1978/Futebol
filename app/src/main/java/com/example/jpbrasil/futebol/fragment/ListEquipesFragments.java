2fd8315e4d3478858a65bc0936333ed594167dd1
jpcm1978 <jpcm1978@hotmail.com>
2017-05-28 15:35:22 -0300

    Modificação:

--- app/src/main/java/com/example/jpbrasil/futebol/fragment/ListEquipesFragments.java
+++ app/src/main/java/com/example/jpbrasil/futebol/fragment/ListEquipesFragments.java
@@ -1,7 +1,12 @@
 package com.example.jpbrasil.futebol.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by JpBrasil on 28/05/2017.
@@ -9,4 +14,9 @@ import android.support.v4.app.Fragment;
 
 public class ListEquipesFragments extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
